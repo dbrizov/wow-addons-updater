6a2f562d727ff1a452815da09585d72fe4e73c76
dbrizov <d.b.rizov@gmail.com>
2020-07-05 09:02:24 +0300

    Added a comment for how to create a build

--- source/setup.py
+++ source/setup.py
@@ -3,3 +3,6 @@ from cx_Freeze import setup, Executable
 setup(name="wow_addons_updater",
       description="WoW Addons Updater",
       executables=[Executable("wow_addons_updater.py")])
+
+# To create a build run the following in the console
+# > python setup.py build
