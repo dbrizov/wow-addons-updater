310f11079a719669ec846930014227fa51572bfb
dbrizov <d.b.rizov@gmail.com>
2022-09-10 12:22:23 +0300

    update setup.py

--- source/setup.py
+++ source/setup.py
@@ -4,5 +4,6 @@ setup(name="wow_addons_updater",
       description="WoW Addons Updater",
       executables=[Executable("wow_addons_updater.py")])
 
-# To create a build run the following in the console
-# > python setup.py build
+# To create a build
+# 1. Install cx_Freeze (pip install cx_Freeze)
+# 2. Run the following in the console "python setup.py build"
